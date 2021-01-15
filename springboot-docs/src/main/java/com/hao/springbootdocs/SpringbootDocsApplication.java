ff5bda8a46d84bb19229b1e5f768b2bf6ce68831
haohj <haohj94@163.com>
2021-01-15 14:39:05 +0800

    springboot整合docs

---
+++ springboot-docs/src/main/java/com/hao/springbootdocs/SpringbootDocsApplication.java
@@ -0,0 +1,13 @@
+package com.hao.springbootdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDocsApplication.class, args);
+	}
+
+}
---
+++ springboot-docs/src/test/java/com/hao/springbootdocs/SpringbootDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.springbootdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

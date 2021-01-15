601c28777fc92f1652da5563d98716a6ab22a613
haohj <haohj94@163.com>
2021-01-15 14:38:08 +0800

    springboot整合task，mail

--- springboot-task/src/test/java/com/hao/task/SpringbootTaskApplicationTests.java
+++ springboot-task/src/test/java/com/hao/task/SpringbootTaskApplicationTests.java
@@ -1,13 +1,51 @@
 package com.hao.task;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class SpringbootTaskApplicationTests {
+    @Autowired
+    JavaMailSenderImpl mailSender;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        //邮件设置
+        message.setSubject("通知-今晚开会");
+        message.setText("今晚7:30开会");
 
+        message.setTo("haohj94@163.com");
+        message.setFrom("1195654629@qq.com");
+
+        mailSender.send(message);
+    }
+
+    @Test
+    public void test02() throws Exception {
+        //1、创建一个复杂的消息邮件
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        //邮件设置
+        helper.setSubject("通知-今晚开会");
+        helper.setText("<b style='color:red'>今天 7:30 开会</b>", true);
+
+        helper.setTo("haohj94@163.com");
+        helper.setFrom("1195654629@qq.com");
+
+        //上传文件
+        helper.addAttachment("1.jpg", new File("C:\\Users\\lfy\\Pictures\\Saved Pictures\\1.jpg"));
+        helper.addAttachment("2.jpg", new File("C:\\Users\\lfy\\Pictures\\Saved Pictures\\2.jpg"));
+
+        mailSender.send(mimeMessage);
+
+    }
 }

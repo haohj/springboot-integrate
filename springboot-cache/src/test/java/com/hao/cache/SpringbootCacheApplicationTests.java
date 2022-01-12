39817194e4cc698e511be85e57f5bffc7eb398a4
haohj <haohj94@163.com>
2022-01-12 17:28:45 +0800

    springboot整合redis

--- springboot-cache/src/test/java/com/hao/cache/SpringbootCacheApplicationTests.java
+++ springboot-cache/src/test/java/com/hao/cache/SpringbootCacheApplicationTests.java
@@ -10,53 +10,62 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class SpringbootCacheApplicationTests {
-	@Autowired
-	EmployeeMapper employeeMapper;
+    @Autowired
+    EmployeeMapper employeeMapper;
 
-	@Autowired
-	StringRedisTemplate stringRedisTemplate;  //操作k-v都是字符串的
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;  //操作k-v都是字符串的
 
-	@Autowired
-	RedisTemplate redisTemplate;  //k-v都是对象的
+    @Autowired
+    RedisTemplate redisTemplate;  //k-v都是对象的
 
-	@Autowired
-	RedisTemplate<Object, Employee> empRedisTemplate;
+    @Autowired
+    RedisTemplate<Object, Employee> empRedisTemplate;
 
-	@Test
-	void contextLoads() {
-		Employee empById = employeeMapper.getEmpById(1);
-		System.out.println(empById);
-	}
+    @Test
+    void contextLoads() {
+        Employee empById = employeeMapper.getEmpById(1);
+        System.out.println(empById);
+    }
 
-	/**
-	 * Redis常见的五大数据类型
-	 *  String（字符串）、List（列表）、Set（集合）、Hash（散列）、ZSet（有序集合）
-	 *  stringRedisTemplate.opsForValue()[String（字符串）]
-	 *  stringRedisTemplate.opsForList()[List（列表）]
-	 *  stringRedisTemplate.opsForSet()[Set（集合）]
-	 *  stringRedisTemplate.opsForHash()[Hash（散列）]
-	 *  stringRedisTemplate.opsForZSet()[ZSet（有序集合）]
-	 */
-	@Test
-	public void test01(){
-		//给redis中保存数据
-		//stringRedisTemplate.opsForValue().append("msg","hello");
+    /**
+     * Redis常见的五大数据类型
+     * String（字符串）、List（列表）、Set（集合）、Hash（散列）、ZSet（有序集合）
+     * stringRedisTemplate.opsForValue()[String（字符串）]
+     * stringRedisTemplate.opsForList()[List（列表）]
+     * stringRedisTemplate.opsForSet()[Set（集合）]
+     * stringRedisTemplate.opsForHash()[Hash（散列）]
+     * stringRedisTemplate.opsForZSet()[ZSet（有序集合）]
+     */
+    @Test
+    public void test01() {
+        //给redis中保存数据
+        //stringRedisTemplate.opsForValue().append("msg","hello");
 //		String msg = stringRedisTemplate.opsForValue().get("msg");
 //		System.out.println(msg);
 
-//		stringRedisTemplate.opsForList().leftPush("mylist","1");
-//		stringRedisTemplate.opsForList().leftPush("mylist","2");
-	}
+        stringRedisTemplate.opsForList().rightPush("mylist", "1");
+        stringRedisTemplate.opsForList().rightPush("mylist", "2");
+    }
 
-	//测试保存对象
-	@Test
-	public void test02(){
-		Employee empById = employeeMapper.getEmpById(1);
-		//默认如果保存对象，使用jdk序列化机制，序列化后的数据保存到redis中
-		//redisTemplate.opsForValue().set("emp-01",empById);
-		//1、将数据以json的方式保存
-		//(1)自己将对象转为json
-		//(2)redisTemplate默认的序列化规则；改变默认的序列化规则；
-		empRedisTemplate.opsForValue().set("emp-01",empById);
-	}
+    //测试保存对象
+    @Test
+    public void test02() {
+        Employee empById = employeeMapper.getEmpById(1);
+        //默认如果保存对象，使用jdk序列化机制，序列化后的数据保存到redis中
+        //redisTemplate.opsForValue().set("emp-01",empById);
+        //1、将数据以json的方式保存
+        //(1)自己将对象转为json
+        //(2)redisTemplate默认的序列化规则；改变默认的序列化规则；
+        empRedisTemplate.opsForValue().set("emp-01", empById);
+    }
+
+    @Test
+    public void test03() {
+        Long size = stringRedisTemplate.opsForList().size("mylist");
+        for (int i = 0; i < size; i++) {
+            String value = stringRedisTemplate.opsForList().leftPop("mylist");
+            System.out.println(value);
+        }
+    }
 }
